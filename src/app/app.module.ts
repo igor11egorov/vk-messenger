bc6f89c00470e3837c9838ef62ff93835a7e0234
Nizami Amirov <n.amirov@mobigility.com>
2021-11-05 19:54:35 +0300

    Refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import {BrowserModule} from '@angular/platform-browser'
 import {NgModule} from '@angular/core'
 
 import {AppComponent} from './app.component'
-import {FormsModule} from '@angular/forms'
+import {FormsModule} from '@angular/forms';
+import { UserItemComponent } from './user-item/user-item.component';
+import { UserListComponent } from './user-list/user-list.component'
 
 @NgModule({
   declarations: [
     AppComponent,
+    UserItemComponent,
+    UserListComponent,
   ],
   imports: [
     BrowserModule,
