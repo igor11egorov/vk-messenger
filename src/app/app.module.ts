770a9ce3ae618a81d877702d39ee8003f556e164
igor11egorov <igor11egorov@gmail.com>
2021-11-15 20:15:15 +0300

    Добавление компоненты Беседа

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import {NgModule} from '@angular/core'
 import {AppComponent} from './app.component'
 import {FormsModule} from '@angular/forms';
 import { UserItemComponent } from './user-item/user-item.component';
-import { UserListComponent } from './user-list/user-list.component'
+import { UserListComponent } from './user-list/user-list.component';
+import { ConversationListComponent } from './conversation-list/conversation-list.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     UserItemComponent,
     UserListComponent,
+    ConversationListComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/conversation-list/conversation-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-conversation-list',
+  templateUrl: './conversation-list.component.html',
+  styleUrls: ['./conversation-list.component.scss']
+})
+export class ConversationListComponent implements OnInit {
+
+  constructor() { }
+
+  message = ''
+
+  onEnter(){
+    
+  }
+
+    ngOnInit(): void {
+  }
+
+}
